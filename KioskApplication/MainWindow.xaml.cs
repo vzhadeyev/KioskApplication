92777baabe564e6d17c8d960bd9ad1fe4d9b9fff
Вячеслав Жадеев <slava.zh@outlook.com>
2016-10-18 20:28:56 +0600

    1. UI changed according to task 2. Print Button 3. Download file synchronously to AppData/Temp

--- KioskApplication/MainWindow.xaml.cs
+++ KioskApplication/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using KioskApplication.ViewModels;
+using System;
 using System.ComponentModel;
+using System.IO;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -26,5 +29,42 @@ namespace KioskApplication
             ViewModel.Pin += value;
         }
 
+        string path;
+
+        private void ButtonPrint_Click(object sender, RoutedEventArgs e)
+        {
+            //4425
+            var name = ViewModel.Pin + ".pdf";
+            var saveFileDirectory = Path.GetTempPath();
+            var saveFileName = Path.Combine(saveFileDirectory, name);
+            path = saveFileName;
+            using (var client = new WebClient())
+            {
+                Uri uri = new Uri("http://arman.39ma.ru/files/" + name);
+                client.DownloadFile(uri, saveFileDirectory+name); // synchronusly download file cause we don't want to allow user interract with UI 
+            }
+
+            ResetView();
+        }
+
+        private void ResetView()
+        {
+            ViewModel.Pin = string.Empty;
+        }
+
+        private void WebClientDownloadCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            MessageBox.Show(path);
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BackSpaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Pin = ViewModel.Pin != string.Empty ? ViewModel.Pin.Remove(ViewModel.Pin.Length - 1) : string.Empty;
+        }
     }
 }
